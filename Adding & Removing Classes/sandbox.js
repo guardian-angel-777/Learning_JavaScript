306300200b0aafa5625ae6955497f4d1c1e78478
Guardian_Angel_777 <61491149+guardian-angel-777@users.noreply.github.com>
2020-03-02 17:58:19 +0530

    Add files via upload

---
+++ Adding & Removing Classes/sandbox.js
@@ -0,0 +1,6 @@
+const content = document.querySelector('p');
+
+console.log(content.classList);
+content.classList.add('error');
+content.classList.remove('error');
+content.classList.add('success');
