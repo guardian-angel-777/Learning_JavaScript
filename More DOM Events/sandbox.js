c0bc814ef4e60f6f06ab261e2a3b2e5e310bf5c4
Guardian_Angel_777 <61491149+guardian-angel-777@users.noreply.github.com>
2020-03-11 14:56:43 +0530

    Add files via upload

---
+++ More DOM Events/sandbox.js
@@ -0,0 +1,20 @@
+const copy = document.querySelector('.copy-me');
+
+copy.addEventListener('copy', () => {
+    console.log("OI my content is copyright")
+    alert("My content is copyright")
+});
+
+const box = document.querySelector('.box');
+
+box.addEventListener('mousemove', e => {
+    //console.log(e);
+    //console.log(e.offsetX, e.offsetY);
+    box.textContent = `x pos- ${e.offsetX} y pos- ${e.offsetY}`;
+});
+
+document.addEventListener('wheel',e => {
+    console.log(e);
+    console.log(e.pageX,e.pageY);
+    
+})
